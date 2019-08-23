97c3ace51a34dd75cf953f10f6594d89e050daf9
Alan Krajina <alankrajina88@gmail.com>
2019-08-23 15:53:23 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name])
     if user && params[:name] != "" #user.authenticate(params[:session][:name])
     #  log_in user
-      redirect_to user
+      redirect_to '/'
     else
       render 'new'
     end
