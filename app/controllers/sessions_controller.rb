90a44d8b75e05b1d12dba8a943f022f7fff4032a
Alan Krajina <alankrajina88@gmail.com>
2019-08-23 15:57:09 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     session[:name] = params[:name]
-    if session[:name] != nil && session[:name] != "" #user.authenticate(params[:session][:name])
+    if session[:name] != nil && session[:name] != ""
     #  log_in user
       redirect_to '/'
     else
