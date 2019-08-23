2670ea18b83dcbc4f28666d5de1fd6a3e3ba34d7
Alan Krajina <alankrajina88@gmail.com>
2019-08-23 15:48:05 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:name])
       log_in user
       redirect_to user
     else
